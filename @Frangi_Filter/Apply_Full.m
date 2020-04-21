99b2ff692f37c2ded45c76eb64a4ad7fb1009c00
Johannes Rebling <johannesrebling@gmail.com>
2020-04-21 09:59:25 +0200

    adding Apply_Full to process same way as GUI without GUI

---
+++ @Frangi_Filter/Apply_Full.m
@@ -0,0 +1,115 @@
+function [filtIm] = Apply_Full(FF, unFilt)
+  % applies Frangi filtering in the same way as the GUI does, but does
+  % not require the GUI.
+  % Difference vs. simple .Apply :
+  %
+
+  try
+    % check if we actually have data to work with
+    if nargin == 1
+      unFilt = FF.raw;
+    end
+
+    if isempty(unFilt)
+      return;
+    end
+
+    % get basic data we always need for frangi filtering
+    unFilt = normalize(unFilt);
+
+    % useScales in micrometer / pixels
+    sigmas = sort(FF.useScales);
+    sigmas = double(sigmas);
+    nSigmas = length(sigmas);
+
+    % NOTE for the scale filtering, we only use default values, they work fine
+    if FF.doClaheScales || FF.doContrastScales
+      IMF = Image_Filter(); % init Image filter class
+      % setup solid default clahe settings
+      IMF.claheNBins = 256;
+      IMF.claheLim = FF.claheSensitivity;
+      IMF.claheNTiles = [32 32];
+      % setup solid default contrast settings
+      IMF.imadLimOut = [0 1];
+      IMF.imadAuto = true;
+      IMF.imadGamme = FF.contrastGamma;
+    end
+
+    FF.filt = zeros(size(unFilt), 'like', unFilt); % filtered image
+    FF.filtScales = zeros([size(unFilt) nSigmas], 'like', unFilt); % fitlered scales
+
+    % Actual Frangi Filtering happening here... %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % start progress bar so we see some progress here
+    barText = sprintf('Filtering using %i scales', nSigmas);
+    progressbar(barText);
+
+    for iScale = 1:nSigmas
+      progressbar(iScale ./ nSigmas);
+
+      iSigma = sigmas(iScale) / 6; % FIXME why the 6 here???
+
+      iFilt = imgaussfilt(unFilt, iSigma, 'FilterSize', 2 * ceil(3 * iSigma) + 1);
+      iFilt = builtin("_fibermetricmex", iFilt, FF.sensitivity, ~FF.invert, iSigma);
+
+      % iFilt can be all zeros depending on filter, if that is the case,
+      % continue as filtScales is already initialized with zeros...
+      if ~any(iFilt(:))
+        continue;
+      end
+
+      % we have data, do something with it...
+      if FF.doClaheScales
+        IMF.filt = iFilt;
+        iFilt = IMF.Apply_CLAHE();
+      end
+
+      if FF.doContrastScales
+        IMF.filt = iFilt;
+        iFilt = IMF.Adjust_Contrast();
+      end
+
+      FF.filtScales(:, :, iScale) = iFilt;
+    end
+
+    % Postprocessing of Frangi-Filtered images ..%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % calculate overall frangi filtered image by taking max of all scales...
+    % then apply potential contrast adjustments
+    % NOTE more advanced combinations of Frangi and Raw @ .Update_Frangi_Combo.
+
+    if ~isempty(FF.filtScales)
+      % only take selected scales into account
+      FF.filt = squeeze(max(FF.filtScales, [], 3));
+      FF.filt = normalize(FF.filt);
+
+      if FF.doPostClahe || FF.doPostContrast
+        FF.filt = IMF.filt; % transfer image data back ...
+
+        if doClahe
+          % setup solid default clahe settings
+          IMF.Apply_CLAHE();
+        end
+
+        if doContrast
+          % setup solid default contrast settings
+          IMF.Adjust_Contrast();
+        end
+
+        FF.filt = IMF.filt; % transfer image data back ...
+        FF.filt = normalize(FF.filt);
+      end
+
+    end
+
+    if FF.verbosePlotting
+      FF.Show();
+    end
+
+    if nargout
+      filtIm = FF.filt;
+    end
+
+  catch me
+    rethrow(me);
+  end
+
+end
--- @Frangi_Filter/Show.m
+++ @Frangi_Filter/Show.m
@@ -1,35 +1,43 @@
 function [] = Show(F)
   % handle figures
   if F.verboseOutput
-    fprintf('[Frangi] Plotting frangi filtered images...');
+    fprintf('[Frangi] Plotting frangi filtered images ...');
   end
-  figure() % always create scales in seperate figure
 
+  figure('Name', 'Frangi Scale Overview') % always create scales in seperate figure
+  tiledlayout('flow', 'TileSpacing', 'compact');
+  t = {};
   % find number of scales for plotting
-  [m,n] = find_subplot_dividers(F.nScales+1); % +1 to always show orig raw image
-
   % always plot raw image that was used for filtering
-  subplot_tight(m,n,1);
-    montage = imfuse(F.raw,F.filt,'montage');
-    imshow(montage);
-  title('Unfiltered vs filtered raw image');
+  t{end + 1} = nexttile();
+  imagescj(F.raw);
+  title('Unfiltered raw image');
 
-  for iPlot = 1:F.nScales
+  t{end + 1} = nexttile();
+  imagescj(F.filt);
+  title('Frangi result');
+
+  for iPlot = 1:size(F.filtScales, 3)
     % fprintf(['    Plotting frangi scale ', num2str(iPlot) , '...\n']);
-    filtImage = F.filtScales(:,:,iPlot);
+    filtImage = F.filtScales(:, :, iPlot);
     filtImage = normalize(filtImage);
-    filtImage = adapthisteq(filtImage,'Distribution',F.claheDistr,'NBins',F.claheNBins,...
-      'ClipLimit',F.claheLim,'NumTiles',F.claheNTiles);
+    filtImage = adapthisteq(filtImage, 'Distribution', F.claheDistr, 'NBins', F.claheNBins, ...
+      'ClipLimit', F.claheLim, 'NumTiles', F.claheNTiles);
+    [~, montage] = im_overlay(F.raw, filtImage);
 
-    [~,montage] = im_overlay(F.raw,filtImage);
-    subplot_tight(m,n,iPlot+1);
-      imshow(montage);
-      % don't create new figures for each scale, we have subplots for that
-      % but we have to change the F.newFigPlotting settings as it's used in Overlay_Mask
-      title(sprintf('Scale: %i',iPlot));
+    t{end + 1} = nexttile();
+    imshow(montage);
+    % don't create new figures for each scale, we have subplots for that
+    % but we have to change the F.newFigPlotting settings as it's used in Overlay_Mask
+    titleStr = sprintf('Scale:%i (%2.0f Px)', iPlot, F.autoScales(iPlot));
+    title(titleStr);
   end
+
+  linkaxes([t{3:end}]);
+  linkaxes([t{1:2}]);
 
   if F.verboseOutput
     done();
   end
+
 end
